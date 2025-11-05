e9ba8cfb3e54628169a580dfc55fbd473789ec72
luojunyuan <k1mlka@outlook.com>
2025-11-05 11:35:14 +0800

    添加 UWP 与 Pacakge 空白项目

---
+++ TouchChanX.UWP/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace TouchChanX.UWP
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                button.Content = "Clicked!";
+            }
+        }
+    }
+}
