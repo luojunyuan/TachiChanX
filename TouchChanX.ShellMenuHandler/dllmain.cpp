56d4d8afae18d85874430bbc2e456c05f2d361f2
luojunyuan <k1mlka@outlook.com>
2026-01-22 17:06:00 +0800

    实现 ShellHandler 在 win10 上的显示，并处理了 DLL 导出函数的批注警告（注需要重启 explorer)

--- TouchChanX.ShellMenuHandler/dllmain.cpp
+++ TouchChanX.ShellMenuHandler/dllmain.cpp
@@ -1,5 +1,30 @@
 ﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "pch.h"
+#include <wrl/module.h>
+#include <wrl/implements.h>
+#include <wrl/client.h>
+#include "TouchChanRootCommand.h"
+
+CoCreatableClass(TouchChanRootCommand)
+CoCreatableClassWrlCreatorMapInclude(TouchChanRootCommand)
+
+STDAPI DllGetActivationFactory(_In_ HSTRING activatableClassId, _COM_Outptr_ IActivationFactory** factory)
+{
+    return Microsoft::WRL::Module<Microsoft::WRL::ModuleType::InProc>::GetModule()
+        .GetActivationFactory(activatableClassId, factory);
+}
+
+_Use_decl_annotations_ STDAPI DllCanUnloadNow()
+{
+    return Microsoft::WRL::Module<Microsoft::WRL::InProc>::GetModule()
+        .GetObjectCount() == 0 ? S_OK : S_FALSE;
+}
+
+_Use_decl_annotations_ STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, void** instance)
+{
+    return Microsoft::WRL::Module<Microsoft::WRL::InProc>::GetModule()
+        .GetClassObject(rclsid, riid, instance);
+}
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
