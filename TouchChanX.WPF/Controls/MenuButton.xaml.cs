42b75f6b59886f7abf82d403edda7fd13e4a0b52
kimika <k1mlka@outlook.com>
2026-02-10 18:15:51 +0800

    使用 VSM 实现鼠标侧的 MenuButton 交互

--- TouchChanX.WPF/Controls/MenuButton.xaml.cs
+++ TouchChanX.WPF/Controls/MenuButton.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using FluentIcons.Common;
 using System.Windows;
 using System.Windows.Controls;
+using R3;
+using R3.ObservableEvents;
+using System.Windows.Input;
 
 namespace TouchChanX.WPF.Controls;
 
@@ -30,5 +33,16 @@ public partial class MenuButton : UserControl
     public MenuButton()
     {
         InitializeComponent();
+        
+        this.Events().MouseEnter
+            .Where(e => e is { LeftButton: MouseButtonState.Pressed } or { RightButton: MouseButtonState.Pressed })
+            .Merge(this.Events().MouseDown.Cast<MouseButtonEventArgs, MouseEventArgs>())
+            .Subscribe(_ => 
+                VisualStateManager.GoToState(this, nameof(PressedState), false));
+
+        this.Events().MouseLeave
+            .Merge(this.Events().MouseUp.Cast<MouseButtonEventArgs, MouseEventArgs>())
+            .Subscribe(_ => 
+                VisualStateManager.GoToState(this, nameof(NormalState), false));
     }
 }
