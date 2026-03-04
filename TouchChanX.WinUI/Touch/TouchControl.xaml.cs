9ec0c0f646ce8dc8c5a2a7c37df993866c3bd6d1
kimika <k1mlka@outlook.com>
2026-03-04 12:33:02 +0800

    winui 拖动释放 manipulation

--- TouchChanX.WinUI/Touch/TouchControl.xaml.cs
+++ TouchChanX.WinUI/Touch/TouchControl.xaml.cs
@@ -1,18 +1,10 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using R3;
+using R3.ObservableEvents;
+using System.Numerics;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 namespace TouchChanX.WinUI.Touch;
 
@@ -20,8 +12,58 @@ public sealed partial class TouchControl : UserControl
 {
     public static CornerRadius CircleCornerRadius(double width) => new(width / 2);
 
+    private Size ContainerSize => new(ActualWidth, ActualHeight);
+
+    private Rect TouchRect => new(TouchTransform.X, TouchTransform.Y, TouchBorder.ActualWidth, TouchBorder.ActualHeight);
+
     public TouchControl()
     {
         InitializeComponent();
+
+        TouchBorder.ManipulationMode = ManipulationModes.TranslateX | ManipulationModes.TranslateY;
+
+        var draggingStream =
+            TouchBorder.Events().ManipulationDelta
+            .Select(e => new
+            {
+                Delta = e.Delta.Translation,
+                Args = e,
+            })
+            .Share();
+
+        draggingStream
+            .Select(item => item.Delta)
+            .Subscribe(delta =>
+            {
+                TouchTransform.X += delta.X;
+                TouchTransform.Y += delta.Y;
+            });
+
+        draggingStream
+            .Where(item => PositionCalculator.IsBeyondBoundary(
+                ContainerSize, TouchRect))
+            .Select(item => item.Args)
+            .Subscribe(e => e.Complete());
+
+        TouchBorder.Events().ManipulationCompleted
+            .Select(_ => PositionCalculator.CalculateTouchDockedPosition(
+                ContainerSize, TouchRect, 2))
+            .Subscribe(finalPos =>
+                (TouchTransform.X, TouchTransform.Y) = (finalPos.X, finalPos.Y));
+    }
+}
+
+public static class PointerExtesions
+{
+    extension(PointerRoutedEventArgs pointerEvent)
+    {
+        public Point GetPosition(UIElement? visual = null) =>
+            pointerEvent.GetCurrentPoint(visual).Position;
+    }
+
+    extension(Point)
+    {
+        public static Vector2 operator -(Point p1, Point p2) =>
+           p1.ToVector2() - p2.ToVector2();
     }
 }
