6042b938ed5cbfeb8be7363ea1e8276b7313075b
k1mlka <k1mlka@outlook.com>
2026-03-23 12:11:40 +0800

    实现 MenuButton

---
+++ TouchChanX.WinUI/Controls/MenuButton.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using R3;
+using R3.ObservableEvents;
+
+namespace TouchChanX.WinUI.Controls;
+
+public sealed partial class MenuButton : UserControl
+{
+    public static readonly DependencyProperty SymbolProperty =
+        DependencyProperty.Register(
+            nameof(Symbol),
+            typeof(Symbol),
+            typeof(MenuButton),
+            new PropertyMetadata(Symbol.Emoji2));
+
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register(
+            nameof(Text),
+            typeof(string),
+            typeof(MenuButton),
+            new PropertyMetadata(string.Empty));
+
+    public Symbol Symbol
+    {
+        get => (Symbol)GetValue(SymbolProperty);
+        set => SetValue(SymbolProperty, value);
+    }
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public Observable<Unit> Clicked => field ??=
+        this.Events().PointerReleased
+        .Select(_ => Unit.Default)
+        .Share();
+
+    public MenuButton()
+    {
+        InitializeComponent();
+
+        this.Events().PointerPressed
+            .Merge(this.Events().PointerEntered)
+            .Where(e => e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            .Subscribe(_ =>
+                VisualStateManager.GoToState(this, "Pressed", true));
+
+        this.Events().PointerReleased
+            .Merge(this.Events().PointerExited)
+            .Subscribe(_ =>
+                VisualStateManager.GoToState(this, "Normal", true));
+    }
+}
